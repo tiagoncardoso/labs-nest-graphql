ea11e6ef15c60d8fd50e47a7a24d8f69512780af
Tiago Cardoso <tiago.cardoso@sulamerica.com.br>
2024-05-08 12:06:53 -0300

    chore: short import path

--- src/application/usecase/user/find-user.usecase.ts
+++ src/application/usecase/user/find-user.usecase.ts
@@ -1,12 +1,12 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class FindUserUsecase {
     async execute() {
         return {
             id: 1,
-            name: "John Doe",
-            email: "john@gmail.com",
+            name: 'John Doe',
+            email: 'john@gmail.com',
         }
     }
 }
