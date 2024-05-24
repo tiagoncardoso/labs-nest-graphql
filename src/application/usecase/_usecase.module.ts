fd6f1356d48752f038170d9072ee6672c5e61cfd
Tiago Cardoso <tiago.cardoso@sulamerica.com.br>
2024-05-24 18:25:55 -0300

    fix(_usecase): remove user usecase from imports

--- src/application/usecase/_usecase.module.ts
+++ src/application/usecase/_usecase.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common'
-import { FindUserUsecase } from './user/find-user.usecase'
 import { HealthUsecase } from './health/health.usecase'
 import { CreateProductUsecase } from '@/application/usecase/product/create-product.usecase'
 import { RepositoryModule } from '@/infra/repository/_repository.module'
 import { FindAllProductsUsecase } from '@/application/usecase/product/find-all-products.usecase'
 import { FindProductByIdUsecase } from '@/application/usecase/product/find-product-by-id.usecase'
 
-const useCases = [HealthUsecase, FindUserUsecase, CreateProductUsecase, FindAllProductsUsecase, FindProductByIdUsecase]
+const useCases = [HealthUsecase, CreateProductUsecase, FindAllProductsUsecase, FindProductByIdUsecase]
 
 @Module({
     imports: [RepositoryModule],
